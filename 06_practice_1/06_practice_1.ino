defedb8c01e8861dde0e5955bacad744848b4ddd
minseokey <lms990427@gmail.com>
2022-10-03 16:53:49 +0900

    10/03 assignmnet

---
+++ 06_practice_1/06_practice_1.ino
@@ -0,0 +1,25 @@
+int led = 9;           // the PWM pin the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void set_duty(){
+  
+}
+void set_period(){
+  
+}
+
+
+void loop() {
+
+  
+
+ 
+  
+
+}
